ce9480a28d710b7c014f752100f98fb6dde30972
Ramon Wijnands <rayman747@hotmail.com>
2015-01-07 16:11:24 +0100

    namespace all PostureControl logging

--- src/motionobjectives/PostureControl.cpp
+++ src/motionobjectives/PostureControl.cpp
@@ -26,12 +26,12 @@ void PostureControl::initialize(const KDL::JntArray& q_min, const KDL::JntArray&
         q_max_[i] = q_max(i);
     }
 
-    ROS_INFO("Length joint array = %i",num_joints_);
+    ROS_INFO_NAMED("PostureControl", "PostureControl: Length joint array = %i",num_joints_);
     for (uint i = 0; i < num_joints_; i++) {
         K_[i] = gain[i] / ((q_max(i) - q_min(i))*(q_max(i) - q_min(i)));
-        //ROS_INFO("qmin = %f, qmax = %f, q0 = %f, K = %f, gain = %f",q_min(i),q_max(i),q0_[i], K_[i], gain[i]);
+        //ROS_INFO_NAMED("PostureControl", "PostureControl: qmin = %f, qmax = %f, q0 = %f, K = %f, gain = %f",q_min(i),q_max(i),q0_[i], K_[i], gain[i]);
     }
-    //ROS_INFO("Length joint array = %i",num_joints_);
+    //ROS_INFO_NAMED("PostureControl", "PostureControl: Length joint array = %i",num_joints_);
 
     current_cost_ = 0;
 
@@ -45,7 +45,7 @@ void PostureControl::update(const KDL::JntArray& q_in, Eigen::VectorXd& tau_out)
     for (uint i = 0; i < num_joints_; i++) {
 		double d_tau = K_[i]*(q0_[i] - q_in(i));
         tau_out(i) += d_tau;
-        //ROS_INFO("q(%i) = %f\tq0 = %f\ttau = %f",i,q_in(i),q0_[i],tau_out(i));
+        //ROS_INFO_NAMED("PostureControl", "PostureControl: q(%i) = %f\tq0 = %f\ttau = %f",i,q_in(i),q0_[i],tau_out(i));
 
         // Add this to the costs
         current_cost_ += fabs(d_tau);
@@ -57,7 +57,7 @@ bool PostureControl::setJointTarget(const std::string& joint_name, const double&
 
     // ToDo: return bool
 
-    ROS_DEBUG("Posture controller: setting joint targets");
+    ROS_DEBUG_NAMED("PostureControl", "PostureControl: setting joint targets");
     /// Get joint index
     std::map<std::string, unsigned int>::const_iterator index_iter = joint_name_to_index_.find(joint_name);
     if (index_iter != joint_name_to_index_.end())
@@ -65,15 +65,15 @@ bool PostureControl::setJointTarget(const std::string& joint_name, const double&
         unsigned int index = index_iter->second;
         /// Check whether value is between bounds
         if (value < q_min_[index] || value > q_max_[index]) {
-            ROS_WARN("Joint %i value %f is not between bounds [%f\t%f]", index, value, q_min_[index], q_max_[index]);
+            ROS_WARN_NAMED("PostureControl", "PostureControl: Joint %i value %f is not between bounds [%f\t%f]", index, value, q_min_[index], q_max_[index]);
             return false;
         }
         else {
-            //ROS_INFO("Posture controller: %s (%i) to %f", joint_name.c_str(), (int)index, value);
+            //ROS_INFO_NAMED("PostureControl", "PostureControl: Posture controller: %s (%i) to %f", joint_name.c_str(), (int)index, value);
             q0_[index] = value;
         }
     } else {
-        ROS_ERROR("Joint %s not in posture controller", joint_name.c_str());
+        ROS_ERROR_NAMED("PostureControl", "PostureControl: Joint %s not in posture controller", joint_name.c_str());
         return false;
     }
     return true;
